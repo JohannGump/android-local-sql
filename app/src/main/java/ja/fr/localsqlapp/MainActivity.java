43572327d088022ea9c0db212a751f4af6527391
Robert <robert@gmail.com>
2018-01-10 09:44:16 +0100

    Log

--- app/src/main/java/ja/fr/localsqlapp/MainActivity.java
+++ app/src/main/java/ja/fr/localsqlapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,9 +26,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private List<Map<String,String>> contactList;
     private Integer selectedIndex;
     private Map<String,String> selectedPerson;
+    private final String LIFE_CYCLE = "cycle de vie";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(LIFE_CYCLE,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -169,5 +172,41 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         this.selectedPerson = contactList.get(position);
         Toast.makeText(this, "Ligne " + position + " cliquée", Toast.LENGTH_LONG).show();
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LIFE_CYCLE,"onPause");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LIFE_CYCLE,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LIFE_CYCLE,"onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LIFE_CYCLE,"onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LIFE_CYCLE,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LIFE_CYCLE,"onDestroy");
     }
 }
